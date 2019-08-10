8aaf17ad596f6e7fb38b240ad63fac40f6d0d2ea
neko-0 <yuzhan.physics@gmail.com>
2019-08-10 05:07:46 -0700

    different way to retrieve stuff

---
+++ BetaScope_Driver/src/BetaScope.cpp
@@ -0,0 +1,26 @@
+#include "BetaScope_Driver/include/BetaScope_Class.h"
+
+#include "Colorful_Cout/include/Colorful_Cout.h"
+
+#include <string>
+#include <iostream>
+#include <map>
+
+template <typename dataType>
+dataType* BetaScope::get(std::string key, std::string dtype){
+  if(dtype.compare("vector<double>")==0 )
+  {
+    return NULL;//&this->oTreeVecDoubleMap[key];
+  }
+  else if( dtype.compare("vector<int>")==0 )
+  {
+    return NULL;//&this->oTreeVecIntMap[key];
+  }
+  else
+  {
+      return NULL;
+  }
+};
+
+std::vector<double> *BetaScope::getDV(std::string key){ this->oTreeVecDoubleMap[key]; }
+std::vector<int> *BetaScope::getIV(std::string key){ this->oTreeVecIntMap[key]; }
