c7e102cec5852497148b9874c21821a5bb8605b9
neko-0 <yuzhan.physics@gmail.com>
2019-08-27 16:04:41 -0700

    added neg pmax finder for bunch signal

--- General/WaveformAna/include/Waveform_Analysis.hpp
+++ General/WaveformAna/include/Waveform_Analysis.hpp
@@ -53,6 +53,15 @@ class WaveformAnalysis
       const std::pair<double, unsigned int> Pmax
     );
 
+    void Find_Bunch_Negative_Signal_Maximum(
+      std::vector<double> voltageVec,
+      std::vector<double> timeVec,
+      std::vector<double> pmax,
+      std::vector<double> tmax,
+      std::vector<double> &negPmax,
+      std::vector<double> &negTmax
+    );
+
     //==========================================================================
     // Pulse Area
     double Find_Pulse_Area(
