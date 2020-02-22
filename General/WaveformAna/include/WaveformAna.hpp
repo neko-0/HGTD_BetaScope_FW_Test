ed9b4337cbcf7facb933d62cb0039a61e362b362
neko-0 <yuzhan.physics@gmail.com>
2020-02-21 16:30:49 -0800

    update setter

--- General/WaveformAna/include/WaveformAna.hpp
+++ General/WaveformAna/include/WaveformAna.hpp
@@ -43,167 +43,47 @@ public:
       const bool &tResample, const float &xori, const float &dt
     ) : Waveform<data_type, input_type>(v1, v2, invert, ampFactor, tFactor, tResample, xori, dt) {};
 
-    void set_channel(int value)
-    {
-        this->channel_ = value;
-    }
-    void set_pmax(double value)
-    {
-        this->pmax_ = value;
-    }
-    void set_tmax(double value)
-    {
-        this->tmax_ = value;
-    }
-    void set_max_index(int value)
-    {
-        this->max_index_ = value;
-    }
-    void set_neg_pmax(double value)
-    {
-        this->neg_pmax_ = value;
-    }
-    void set_neg_tmax(double value)
-    {
-        this->neg_tmax_ = value;
-    }
-    void set_neg_max_index(int value)
-    {
-        this->neg_max_index_ = value;
-    }
-    void set_rise_time(double value)
-    {
-        this->rise_time_ = value;
-    }
-    void set_fall_time(double value)
-    {
-        this->fall_time_ = value;
-    }
-    void set_pulse_area(double value)
-    {
-        this->pulse_area_ = value;
-    }
-    void set_pulse_area_undershoot(double value)
-    {
-        this->pulse_area_undershoot_ = value;
-    }
-    void set_dvdt(std::vector<double> value)
-    {
-        this->dvdt_ = value;
-    }
-    void set_cfd(std::vector<double> value)
-    {
-        this->cfd_ = value;
-    }
-    void set_cfd_fall(std::vector<double> value)
-    {
-        this->cfd_fall_ = value;
-    }
-    void set_fine_cfd(std::vector<double> value)
-    {
-        this->fine_cfd_ = value;
-    }
-    void set_fine_cfd_fall(std::vector<double> value)
-    {
-        this->fine_cfd_fall_ = value;
-    }
-    void set_front_baseline_int(double value)
-    {
-        this->front_baseline_int_ = value;
-    }
-    void set_back_baseline_int(double value)
-    {
-        this->back_baseline_int_ = value;
-    }
-    void set_threashold_time(std::vector<double> value)
-    {
-        this->threshold_time_ = value;
-    }
-    void set_rms(double value)
-    {
-        this->rms_ = value;
-    }
+    void set_channel(const int &value){ this->channel_ = value; }
+    void set_pmax(const double &value){ this->pmax_ = value; }
+    void set_tmax(const double &value){ this->tmax_ = value; }
+    void set_max_index(const int &value){ this->max_index_ = value; }
+    void set_neg_pmax(const double &value){ this->neg_pmax_ = value; }
+    void set_neg_tmax(const double &value){ this->neg_tmax_ = value; }
+    void set_neg_max_index(const int &value){ this->neg_max_index_ = value; }
+    void set_rise_time(const double &value){ this->rise_time_ = value; }
+    void set_fall_time(const double &value){ this->fall_time_ = value; }
+    void set_pulse_area(const double value){ this->pulse_area_ = value; }
+    void set_pulse_area_undershoot(const double &value){ this->pulse_area_undershoot_ = value; }
+    void set_dvdt(const std::vector<double> &value){ this->dvdt_ = value; }
+    void set_cfd(const std::vector<double> &value){ this->cfd_ = value; }
+    void set_cfd_fall(const std::vector<double> &value){ this->cfd_fall_ = value; }
+    void set_fine_cfd(const std::vector<double> &value){ this->fine_cfd_ = value; }
+    void set_fine_cfd_fall(const std::vector<double> &value){ this->fine_cfd_fall_ = value; }
+    void set_front_baseline_int(const double &value){ this->front_baseline_int_ = value; }
+    void set_back_baseline_int(const double &value){ this->back_baseline_int_ = value; }
+    void set_threashold_time(const std::vector<double> &value){ this->threshold_time_ = value; }
+    void set_rms(const double &value){ this->rms_ = value; }
 
-    int get_channel()
-    {
-        return this->channel_;
-    }
-    int get_max_index()
-    {
-        return this->max_index_;
-    }
-    double get_pmax()
-    {
-        return this->pmax_;
-    }
-    double get_tmax()
-    {
-        return this->tmax_;
-    }
-    int get_meg_max_index()
-    {
-        return this->neg_max_index_;
-    }
-    double get_neg_pmax()
-    {
-        return this->neg_pmax_;
-    }
-    double get_neg_tmax()
-    {
-        return this->neg_tmax_;
-    }
-    double get_rise_time()
-    {
-        return this->rise_time_;
-    }
-    double get_fall_time()
-    {
-        return this->fall_time_;
-    }
-    double get_pulse_area()
-    {
-        return this->pulse_area_;
-    }
-    double get_pulse_area_undershoot()
-    {
-        return this->pulse_area_undershoot_;
-    }
-    std::vector<double> get_dvdt()
-    {
-        return this->dvdt_;
-    }
-    std::vector<double> get_cfd()
-    {
-        return this->cfd_;
-    }
-    std::vector<double> get_cfd_fall()
-    {
-        return this->cfd_fall_;
-    }
-    std::vector<double> get_fine_cfd()
-    {
-        return this->fine_cfd_;
-    }
-    std::vector<double> get_fine_cfd_fall()
-    {
-        return this->fine_cfd_fall_;
-    }
-    double get_front_baseline_int()
-    {
-        return this->front_baseline_int_;
-    }
-    double get_back_baseline_int()
-    {
-        return this->back_baseline_int_;
-    }
-    std::vector<double> get_threshold_time()
-    {
-        return this->threshold_time_;
-    }
-    double get_rms()
-    {
-        return this->rms_;
-    }
+    int get_channel(){ return this->channel_; }
+    int get_max_index(){ return this->max_index_; }
+    double get_pmax(){ return this->pmax_; }
+    double get_tmax(){ return this->tmax_; }
+    int get_meg_max_index(){ return this->neg_max_index_; }
+    double get_neg_pmax(){ return this->neg_pmax_; }
+    double get_neg_tmax(){ return this->neg_tmax_; }
+    double get_rise_time(){ return this->rise_time_; }
+    double get_fall_time(){ return this->fall_time_; }
+    double get_pulse_area(){ return this->pulse_area_; }
+    double get_pulse_area_undershoot(){ return this->pulse_area_undershoot_; }
+    std::vector<double> get_dvdt(){ return this->dvdt_; }
+    std::vector<double> get_cfd(){ return this->cfd_; }
+    std::vector<double> get_cfd_fall(){ return this->cfd_fall_; }
+    std::vector<double> get_fine_cfd(){ return this->fine_cfd_; }
+    std::vector<double> get_fine_cfd_fall(){ return this->fine_cfd_fall_; }
+    double get_front_baseline_int(){ return this->front_baseline_int_; }
+    double get_back_baseline_int(){ return this->back_baseline_int_; }
+    std::vector<double> get_threshold_time(){ return this->threshold_time_; }
+    double get_rms(){ return this->rms_; }
 
     ClassDef(WaveformAna, 1)
 };
