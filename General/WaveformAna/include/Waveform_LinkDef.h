fdae5f7a2aa7f5fe2e2b932acce05f27e0e97c8a
neko-0 <yuzhan.physics@gmail.com>
2020-04-11 05:26:19 -0700

    fixed unused class in LinkDef

---
+++ General/WaveformAna/include/Waveform_LinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ defined_in "./Waveform.hpp";
+
+#pragma link C++ class Waveform<double,double>+;
+#endif
