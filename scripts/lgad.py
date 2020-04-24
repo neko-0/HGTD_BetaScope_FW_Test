d9ac089a757b41a725ccd791267e4c0a8199f543
Yuzhan <yuzhao@ucsc.edu>
2020-04-23 17:51:25 -0700

    update flag

--- scripts/lgad.py
+++ scripts/lgad.py
@@ -240,7 +240,7 @@ class Lgad(cmd.Cmd, object):
                         p.wait()
             else:
                 if not nohup:
-                    p = subprocess.Popen("{nohup} $BETASCOPE_SCRIPTS/../BetaScope_Ana/BetaScopeWaveformAna/bin/Run_WaveformAna {tdir}/WaveformAnaConfig.ini --skipWaveform {nohup_log}".format(nohup=nohup, nohup_log=nohup_log, tdir=self.current_run), shell=True)
+                    p = subprocess.Popen("{nohup} $BETASCOPE_SCRIPTS/../BetaScope_Ana/BetaScopeWaveformAna/bin/Run_WaveformAna -config {tdir}/WaveformAnaConfig.ini --skipWaveform {nohup_log}".format(nohup=nohup, nohup_log=nohup_log, tdir=self.current_run), shell=True)
                     p.wait()
 
                     if "full" in mode:
@@ -253,7 +253,7 @@ class Lgad(cmd.Cmd, object):
                         p.wait()
                 else:
                     def nohupRun(mode):
-                        p = subprocess.Popen("{nohup} $BETASCOPE_SCRIPTS/../BetaScope_Ana/BetaScopeWaveformAna/bin/Run_WaveformAna {tdir}/WaveformAnaConfig.ini --skipWaveform {nohup_log}".format(nohup=nohup, nohup_log=nohup_log, tdir=self.current_run), shell=True)
+                        p = subprocess.Popen("{nohup} $BETASCOPE_SCRIPTS/../BetaScope_Ana/BetaScopeWaveformAna/bin/Run_WaveformAna -config {tdir}/WaveformAnaConfig.ini --skipWaveform {nohup_log}".format(nohup=nohup, nohup_log=nohup_log, tdir=self.current_run), shell=True)
                         #pid = p.pid
                         #isRunning(pid)
                         p.wait()
