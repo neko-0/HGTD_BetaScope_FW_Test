572fc974f7966795a10ed2391fdc25e4f3e4f0c5
neko-0 <yuzhan.physics@gmail.com>
2020-05-22 13:29:27 -0700

    update output ini file naming when there's multiple cycle

--- scripts/betaScopePlot/main/main_getResults.c
+++ scripts/betaScopePlot/main/main_getResults.c
@@ -95,19 +95,22 @@ void result( PlotConfigMgr::ConfigSection sec, int dut_channel, int trigger_chan
   std::lock_guard<std::mutex> lck(MTX);
   DataOutputFormat outfile;
   std::string biasVoltage;
+  std::string biasVoltage2;
   std::string myBuffer = sec.file_name;
   if(myBuffer.find(".root.")!=std::string::npos)
   {
     std::string fIndex;
     fIndex = myBuffer.substr(myBuffer.find(".root.")+5, myBuffer.length() );
     biasVoltage = sec.bias + "." + fIndex;
+    biasVoltage2 = sec.bias + "findex" + fIndex;
   }
   else
   {
     biasVoltage = sec.bias;
+    biasVoltage2 = sec.bias;
   }
   outfile.CreateBetaScopeOutputFile( biasVoltage.c_str(), oData, sec.temperature, sec.trigger_bias );
-  outfile.ParseRawOutputToINI(biasVoltage, oData, sec.temperature );
+  outfile.ParseRawOutputToINI(biasVoltage2, oData, sec.temperature );
 
   fmt::print("{} is Finished.\n", sec.file_name);
   my_cut_v.clear();
