4baf4cafcbabda6e964a5a8e5c681c5ba9b48774
neko-0 <30849691+neko-0@users.noreply.github.com>
2020-08-24 06:37:11 -0700

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,6 @@
-tar -xvf test_run.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qCYdSienjzc9QlKpNPY3AJHNplcROWNr' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qCYdSienjzc9QlKpNPY3AJHNplcROWNr" -O test_run.zip && rm -rf /tmp/cookies.tx
 
-./BetaScope_Ana/BetaScopeWaveformAna/bin/GenerateWaevformCofing
+unzip test_run.zip
+
+./BetaScope_Ana/BetaScopeWaveformAna/bin/GenerateWaevformCofing test
 ./BetaScope_Ana/BetaScopeWaveformAna/bin/Run_WaveformAna -config WaveformAnaConfig.ini
