a8b1d36440ac767b2e178f5518ff314a3c845f0f
neko-0 <30849691+neko-0@users.noreply.github.com>
2020-08-24 06:39:17 -0700

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qCYdSienjzc9QlKpNPY3AJHNplcROWNr' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qCYdSienjzc9QlKpNPY3AJHNplcROWNr" -O test_run.zip && rm -rf /tmp/cookies.tx
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qCYdSienjzc9QlKpNPY3AJHNplcROWNr' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qCYdSienjzc9QlKpNPY3AJHNplcROWNr" -O raw.zip && rm -rf /tmp/cookies.tx
 
-unzip test_run.zip
+unzip raw.zip
 
-./BetaScope_Ana/BetaScopeWaveformAna/bin/GenerateWaevformCofing test
+./BetaScope_Ana/BetaScopeWaveformAna/bin/GenerateWaevformCofing
 ./BetaScope_Ana/BetaScopeWaveformAna/bin/Run_WaveformAna -config WaveformAnaConfig.ini
